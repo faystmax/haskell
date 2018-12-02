03cb84b875365bf75d18d5040df32888a8463545
maximus <faystmax@gmail.com>
2018-12-02 21:55:53 +0700

    clear

--- src/Nod.hs
+++ src/Nod.hs
@@ -5,7 +5,6 @@ nodSingle a 0 = a
 nodSingle a b = nodSingle b (rem a b)
 
 nod :: [Int] -> Int
-nod [] =  0
 nod (h:[]) =  h
-nod (h:t) = nod(nodSingle h  (head t) : tail(t))
+nod (h:t) = nod(nodSingle h (head t) : tail t)
 
