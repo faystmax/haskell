a15ba084e45a6501055792515ff01817dd029b00
maximus <faystmax@gmail.com>
2018-12-05 00:35:15 +0700

    ping

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,38 @@
 module Main where
 
-import Nod
+import Control.Concurrent
+import Control.Monad (forever)
+import Control.Concurrent.Chan
 
-main :: IO ()
+pinger chanPingPong chanPongPing = do
+    putStrLn "Ping: Pinging"
+    writeChan chanPingPong "ping"
+    putStrLn "Ping: Waiting for a pong"
+    reply <- readChan chanPongPing
+    putStrLn $ "Ping: " ++ reply ++ " received, done"
+
+ponger chanPingPong chanPongPing = do
+    putStrLn "Pong: Waiting for a ping"
+    getPing <- readChan chanPingPong
+    putStrLn $ "Pong: " ++ getPing ++ " received, sending pong"
+    writeChan chanPongPing "pong"
+    putStrLn "Pong: done"
+
+forkCreater action = do
+    forkIO $ forever action
+
+
+main :: IO()
 main = do
-    print (nod [10,30,10])
+    putStrLn "START!"
+    -- создание каналов
+    chanPingPong <- newChan
+    chanPongPing <- newChan
+    -- создание процессов и передача им каналов
+    forkCreater $ pinger chanPingPong chanPongPing
+    forkCreater $ ponger chanPingPong chanPongPing
+    -- ожидание завершения (нажатия enter)
+    getLine
+    return ()
+
 
